f05a126d73d68de2a034e1f169b3448ad10c5cd5
nyroDev <cedric@nyrodev.com>
2011-12-05 17:08:08 +0000

    Fix unchecking all box in checlAll.js

--- nyro/module/compress/js/checkAll.js
+++ nyro/module/compress/js/checkAll.js
@@ -1,6 +1,6 @@
 jQuery(function($) {
 	$('#checkAll').change(function() {
 		var me = $(this);
-		me.closest('form').find('input[type=checkbox]').attr('checked', me.attr('checked'));
+		me.closest('form').find('input[type=checkbox]').attr('checked', me.is(':checked'));
 	});
 });
