901e836cdf49d3151bc2bba7786a09ec907fa9dd
Nyro <cedric@nyrodev.com>
2012-01-26 18:39:44 +0100

    Remove controller in URL when using tinybrowser

--- nyro/lib/tinyMce/plugins/tinybrowser/config_nyroFwk.php
+++ nyro/lib/tinyMce/plugins/tinybrowser/config_nyroFwk.php
@@ -16,9 +16,9 @@ $tinybrowser['path']['file']  = '/'; // Other files location
 
 $tinybrowser['thumbsrc'] = 'link';
 // File link paths - these are the paths that get passed back to TinyMCE or your application (set to equal the upload path by default)
-$tinybrowser['link']['image'] = request::uploadedUri('tinyBrowser/images/'); //$tinybrowser['path']['image']; // Image links
-$tinybrowser['link']['media'] = request::uploadedUri('tinyBrowser/media/'); //$tinybrowser['path']['media']; // Media links
-$tinybrowser['link']['file']  = request::uploadedUri('tinyBrowser/'); //$tinybrowser['path']['file']; // Other file links
+$tinybrowser['link']['image'] = request::uploadedUri('tinyBrowser/images/', array('controller'=>false)); //$tinybrowser['path']['image']; // Image links
+$tinybrowser['link']['media'] = request::uploadedUri('tinyBrowser/media/', array('controller'=>false)); //$tinybrowser['path']['media']; // Media links
+$tinybrowser['link']['file']  = request::uploadedUri('tinyBrowser/', array('controller'=>false)); //$tinybrowser['path']['file']; // Other file links
 
 $tinybrowser['imagequality'] = 100;
 $tinybrowser['thumbquality'] = 100;
