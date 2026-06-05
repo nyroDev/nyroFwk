b960557afd2e4055311bc4441a886d462623c05d
nyro <cedric@nyro.dev>
2026-06-05 23:59:58 +0200

    Fix php 8 issue

--- nyro/module/scaffold/controller.class.php
+++ nyro/module/scaffold/controller.class.php
@@ -200,7 +200,7 @@ class module_scaffold_controller extends module_abstract {
 		));
 	}
     
-    protected function updateFilterWhere(db_where $where = null) {
+    protected function updateFilterWhere(?db_where $where = null) {
         return $where;
     }
 	
